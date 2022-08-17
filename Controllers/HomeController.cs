973a9724407415c3611c3ec89e0448094bf10e3a
AmandaMariaFS <amandinhaferre@hotmail.com>
2022-08-17 11:43:51 -0300

    Conteudo da aula 17/08

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,6 +4,13 @@ using ExemploAspNetMvc.Models;
 
 namespace ExemploAspNetMvc.Controllers;
 
+
+public class UserRequest
+{
+    public string Nome { get; set; }
+    public string Email { get; set; }
+}
+
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
@@ -24,6 +31,21 @@ public class HomeController : Controller
     }
 
     public IActionResult PrimeiraAction()
+    {
+        return View();
+    }
+
+    public string TesteQueryString([FromQuery] string q, [FromQuery] string nome)
+    {
+        return $"Chegou aqui {q} e {nome}";
+    }
+
+    public string TesteForm([FromForm] UserRequest userRequest)
+    {
+        return $"Nome: {userRequest.Nome}, E-mail: {userRequest.Email}";
+    }
+
+    public IActionResult Form()
     {
         return View();
     }
